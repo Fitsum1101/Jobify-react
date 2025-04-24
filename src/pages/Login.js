72f5ee33f97a5cb289e7d55fcf258e17288ef205
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-24 23:19:13 +0300

    adding login pages

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginComp from "../compenets/login/Login";
+
+const Login = () => {
+  return (
+    <div className="h-[100vh] flex justify-center items-start bg-[#002A47]">
+      <LoginComp />
+    </div>
+  );
+};
+
+export default Login;
