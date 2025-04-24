40a147de2841fff479bbf867ab3cde4543872f10
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-24 19:44:01 +0300

    adding route demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { createContext } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
+import BooksNew from "./compenets/books/BooksNew";
+import Book from "./compenets/books/Book";
+import Book1 from "./compenets/books/Book1";
 
 const App = () => {
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<h1>home page</h1>} />
+        <Route path="/books" element={<Book1 />}>
+          <Route index="true" element={<h1>book homepage</h1>} />
+          <Route path="new" element={<BooksNew />} />
+          <Route path=":id" element={<Book />} />{" "}
+        </Route>
+      </Routes>
   );
 };
 
