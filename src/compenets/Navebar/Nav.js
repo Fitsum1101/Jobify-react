b70283eb49ac0af10467e3ad53a94d14bfdb92e3
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-29 15:03:45 +0300

    adding navbar page

--- src/compenets/Navebar/Nav.js
+++ src/compenets/Navebar/Nav.js
@@ -3,7 +3,7 @@ import Navicons from "./Navicons";
 
 const Nav = () => {
   return (
-    <div className="bg-[#002A47] p-3 text-center w-[100%] fixed top-0 left-0 border-b-1 shadow-cyan-800 shadow-sm ">
+    <div className="bg-[#002A47] p-3 text-center w-[100%] fixed top-0 left-0   ">
       <div className="flex max-w-[1098px] m-auto justify-between text-white align-middle">
         <h1 className="text-2xl capitalize font-bold ">Jobify</h1>
         <Navicons />
